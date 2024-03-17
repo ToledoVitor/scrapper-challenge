0d49fbed30eb29832c9038ab235c3181f6faea5e
Vitor Toledo <pr.vitortoledo@gmail.com>
2024-03-17 14:50:01 -0300

    add second_page sample

--- tests/test_istock_spider.py
+++ tests/test_istock_spider.py
@@ -18,6 +18,17 @@ class OsdirSpiderTest(unittest.TestCase):
         assert spider.images is not None
         assert len(spider.images) == 60
 
+    def test_parse_second_page(self):
+        spider = IStockSpider(save_on_db=False)
+        spider.parse(
+            fake_response_from_file(
+                f"{self.BASE_PATH}/second_page.html", url="https://www.example.com"
+            )
+        )
+
+        assert spider.images is not None
+        assert len(spider.images) == 60
+
     def test_parse_last_page(self):
         spider = IStockSpider(save_on_db=False)
         spider.parse(
